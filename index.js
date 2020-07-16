fd0f551f04ae90ad3995306aa6b05d9ae30f4415
_Niemals <52979674+NeverLore@users.noreply.github.com>
2020-07-16 22:06:13 +0300

    "JCH"fix

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ bot.on("message", msg => {
 	} else {
 		var atch = msg.attachments.first().url
 	}
-	term(`[ ^y${msg.guild.name}^ / ^y${msg.channel.name}^ ] ^o${msg.author.username}^: ${msg.content}\n${atch}\n`)
+	term(`[ ^y${msg.guild.name}^ / ^y${msg.channel.name}^ ] ^o${msg.author.username}^ > ${msg.content}\n${atch}\n`)
 })});
 
